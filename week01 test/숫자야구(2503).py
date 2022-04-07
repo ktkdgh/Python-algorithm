21205579cfcc017d52799d4138222651bd5b672e
ktkdgh <ktkdgh821@gmail.com>
2022-04-07 22:15:08 +0900

    week01 test 수정

--- week01 test/숫자야구(2503).py
+++ week01 test/숫자야구(2503).py
@@ -1,25 +1,24 @@
-import sys 
-from itertools import permutations 
+import sys
+import itertools
 
-n = int(input()) 
-num = list(permutations([1,2,3,4,5,6,7,8,9], 3)) 
+nPr = list(itertools.permutations([1,2,3,4,5,6,7,8,9], 3))
+num = int(sys.stdin.readline())
 
-for _ in range(n): 
-    test, s, b = map(int, input().split()) 
-    test = list(str(test)) 
-    remove_cnt = 0 
-    for i in range(len(num)): 
-        s_cnt = b_cnt = 0 
-        i -= remove_cnt 
-        for j in range(3): 
-            test[j] = int(test[j]) 
-            if test[j] in num[i]: 
-                if j == num[i].index(test[j]): 
-                    s_cnt += 1 
-                else: 
-                    b_cnt += 1 
-        if s_cnt != s or b_cnt != b: 
-            num.remove(num[i]) 
-            remove_cnt += 1 
-            
-print(len(num))
+for _ in range(num):
+    game, s, b = map(int, sys.stdin.readline().split())
+    game = list(str(game))
+    remove_cnt = 0
+    for i in range(len(nPr)):
+        s_cnt = b_cnt = 0
+        i -= remove_cnt
+        for j in range(3):
+            game[j] = int(game[j])
+            if game[j] in nPr[i]:
+                if j == nPr[i].index(game[j]):
+                    s_cnt += 1
+                else:
+                    b_cnt += 1
+        if s_cnt != s or b_cnt != b:
+            nPr.remove(nPr[i])
+            remove_cnt += 1
+print(len(nPr))
