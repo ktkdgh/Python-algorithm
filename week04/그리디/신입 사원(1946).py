076e64772192d58dd124e8dc3933f71609c15c62
ktkdgh <ktkdgh821@gmail.com>
2022-04-23 22:18:52 +0900

    2022/04/23

---
+++ week04/그리디/신입 사원(1946).py
@@ -0,0 +1,8 @@
+import sys
+
+for _ in range(int(sys.stdin.readline())):
+    n = int(sys.stdin.readline())
+
+    for _ in range(n):
+        s, m = map(int, sys.stdin.readline().split())
+        
