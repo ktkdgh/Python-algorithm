aef029848ab85a4478b9ea7b84e0a255a488735a
ktkdgh <ktkdgh821@gmail.com>
2022-06-12 22:48:33 +0900

    2022/06/12

--- algorithm/LeetCode/14장 트리/47_이진 트리 직렬화&역직렬화(297).py
+++ algorithm/LeetCode/14장 트리/47_이진 트리 직렬화&역직렬화(297).py
@@ -1,4 +1,5 @@
-
+# 실패
+import collections
 
 root = [1,2,3,0,0,4,5]
 # Definition for a binary tree node.
@@ -16,7 +17,21 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        
+
+        queue = collections.deque([root])
+        result = ['#']
+
+        while queue:
+            node = queue.popleft()
+
+            if node:
+                queue.append(node.left)
+                queue.append(node.right)
+
+                result.append(str(node.val))
+            else:
+                result.append('#')
+        return ' '.join(result)
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -24,7 +39,27 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
+        if data == '# #':
+            return None
         
+        nodes = data.split()
+
+        root = TreeNode(int(nodes[1]))
+        queue = collections.deque([root])
+        index = 2
+
+        while queue:
+            node = queue.popleft()
+            if nodes[index] is not '#':
+                node.left = TreeNode(int(nodes[index]))
+                queue.append(node.left)
+            index += 1
+
+            if nodes[index] is not '#':
+                node.right = TreeNode(int(nodes[index]))
+                queue.append(node.right)
+            index += 1
+        return root
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
---
+++ algorithm/LeetCode/14장 트리/49_최소 높이 트리(310).py
@@ -0,0 +1,8 @@
+n = 4
+edges = [[1,0],[1,2],[1,3]]
+
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: list[list[int]]) -> list[int]:
+        return 0
+        
+print(Solution.findMinHeightTrees(0, n, edges))
