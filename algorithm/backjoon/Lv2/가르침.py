28190ed03ff4e9b4527f2a4cfa9a7fea14cfb865
ktkdgh <ktkdgh821@gmail.com>
2022-07-06 00:09:00 +0900

    2022/07/06

--- algorithm/backjoon/Lv2/가르침.py
+++ algorithm/backjoon/Lv2/가르침.py
@@ -1,12 +1,40 @@
 # https://www.acmicpc.net/problem/1062
 
-#
+# 틀림
+from itertools import combinations
 import sys
 
 n, k = map(int, sys.stdin.readline().split())
-word = []
+data = [sys.stdin.readline().strip()[4:-4] for _ in range(n)]
+first_word = {'a', 'n', 't', 'i', 'c'}
+remain_alphabet = set(chr(i) for i in range(97, 123)) - first_word
 
-for i in range(n):
-    word.append(sys.stdin.readline().strip())
+def checkWords(data, learned):
+    cnt = 0
+    for word in data:
+        canRead = 1
+        for w in word:
+            if learned[ord(w)] == 0:
+                canRead = 0
+                break
+        if canRead == 1:
+            cnt += 1
+    return cnt
 
-print(word)
+answer = 0
+if k >= 5:
+    learned = [0] * 123
+    for x in first_word:
+        learned[ord(x)] = 1
+
+    for teach in list(combinations(remain_alphabet, k-5)):
+        for t in teach:
+            learned[ord(t)] = 1
+        cnt = checkWords(data, learned)
+        if cnt > answer:
+            answer = cnt
+        for t in teach:
+            learned[ord(t)] = 0
+    print(answer)
+else:
+    print(0)
--- algorithm/dongbin-book/01_모험가 길드.py
+++ algorithm/dongbin-book/01_모험가 길드.py
@@ -1,3 +1,18 @@
 # url x
 
-#
+# 틀림
+def solution(adventurer):
+    answer = 0
+    count = 0
+    adventurer.sort()
+
+    for i in adventurer:
+        count += 1
+        if count >= i:
+            answer += 1
+            count = 0
+
+    return answer
+
+adventurer = [2, 3, 1, 2, 2]
+print(solution(adventurer))
